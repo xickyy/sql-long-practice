d796a7b949332fda69c5eac89c0330964614de64
ricky <ricky_moore@ymail.com>
2022-09-08 21:45:21 -0700

    half of phase 3

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -17,3 +17,32 @@ values (
   0
 )
 ;
+
+insert into customers (
+    name,
+    phone,
+    email
+)
+values (
+    "Monica",
+    "222-222-2222",
+    "monica@friends.show"
+)
+,
+(
+    "Phoebe",
+    "333-333-3333",
+    "phoebe@friends.show"
+)
+;
+
+UPDATE customers SET points = points + 3 WHERE phone = '333-333-3333';
+
+insert into coffee_orders (
+  is_redeemed
+)
+values 
+(0),
+(0),
+(0)
+;
