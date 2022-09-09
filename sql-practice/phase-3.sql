1f7fc9a24c2cf0876150e1ccf2fab64640d2912a
Aman Kaur <amanpreet_k32@yahoo.com>
2022-09-08 21:14:08 -0700

    phase 3 rachel

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,19 @@
--- Your code here
+insert into customers (
+  name,
+  phone
+)
+values (
+  'Rachel',
+  '111-111-1111'
+)
+;
+
+UPDATE customers SET points = points + 1 WHERE phone = '111-111-1111';
+
+insert into coffee_orders (
+  is_redeemed
+)
+values (
+  0
+)
+;
